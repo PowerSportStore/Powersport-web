d79be057ee57dfddb38671b2534a039de975bf75
PowerSportStore <propowersport@gmail.com>
2026-01-15 19:05:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}
+  },
   server: {
     port: 3000
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });
